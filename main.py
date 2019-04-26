7a55217687373d19a6d59c97477a2d56ddba6841
Mike Maddock <mike.maddock@yahoo.com>
2019-04-25 19:39:46 -0500

    user-signup done

--- main.py
+++ main.py
@@ -48,18 +48,20 @@ def index2():
     if len(username)<3 or len(username)>20 or " " in username:
         username_er="Invalid username(must have no spaces, and be 3-20 characters)"
 
-    if len(pass1)<3 or len(pass1)>20 or " " in pass1 or pass1!=pass2:
+    if len(pass1)<3 or len(pass1)>20 or " " in pass1:
         password_er="Invalid password, passwords must match(no spaces, and be 3-20 characters)"
+    if pass1!=pass2:
+        v_password_er="Passwords do not match"
     
     if not check_email(email) or not ecount(email):
         email_er="Invalid email"
-    def final_check(ue, pe, ee):
-        if ue or pe or ee:
-            return render_template("base.html",usernmame=username, username_er=ue, password_er=pe, email_er=ee)
+    def final_check(ue, pe, ve, ee, ):
+        if ue or pe or ve or ee:
+            return render_template("base.html",username=username, email=email, username_er=ue, password_er=pe, v_password_er=ve, email_er=ee) 
         else:
             return render_template("welcome.html")
 
-    return final_check(username_er,password_er,email_er)
+    return final_check(username_er,password_er, v_password_er, email_er)
     
     
     
